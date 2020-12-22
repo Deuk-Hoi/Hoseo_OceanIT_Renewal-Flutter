464e871525658efcd2cd6afff7c164ad3ff98480
Deuk_Hoi <dh97k@naver.com>
2020-12-23 00:09:56 +0900

    add User Name card

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class Main extends StatelessWidget {
     return MaterialApp(
       title: "Mainpage",
       theme: ThemeData(
-        primaryColor: Colors.lightBlue[100],
+        primaryColor: Color(0xff182034),
       ),
       home: MainPage(),
     );
@@ -26,31 +26,90 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("해양IT융합기술연구소"),
-        centerTitle: true,
-        elevation: 0.0,
-      ),
-      drawer: Drawer(),
-      body: Stack(
-        children: [
-          Image.asset('assets/OceanMain.jpg'),
-          Flexible(
-              child: Container(
-            alignment: Alignment(0.0, 0.0),
-            height: 45,
-            margin: EdgeInsets.only(left: 20, right: 20, top: 15),
-            padding: EdgeInsets.only(left: 20, right: 20),
-            child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                    filled: true,
-                    fillColor: Colors.white,
-                    labelText: "Search")),
-          ))
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text("해양IT융합기술연구소"),
+          centerTitle: true,
+          elevation: 0.0,
+        ),
+        drawer: Drawer(),
+        body: Column(
+          children: [
+            Stack(
+              children: [
+                Image.asset('assets/OceanMain.jpg'),
+                Container(
+                  alignment: Alignment.center,
+                  height: 50,
+                  margin: EdgeInsets.only(left: 5, right: 5, top: 15),
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      hintText: "Search",
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.centerRight,
+                  margin: EdgeInsets.only(right: 30, top: 15),
+                  child: IconButton(
+                    icon: Icon(Icons.search),
+                    onPressed: () {},
+                  ),
+                ),
+                Container(
+                    width: double.infinity,
+                    margin: EdgeInsets.only(right: 20, left: 20, top: 230),
+                    height: 70,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey,
+                            blurRadius: 2.0,
+                            spreadRadius: 0.0,
+                            offset: Offset(2.0, 2.0),
+                          )
+                        ]),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(left: 20.0, right: 16.0),
+                          child: CircleAvatar(
+                            child: Text("득"),
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(top: 5),
+                              child: Text(
+                                "AI 융합대학 컴퓨터공학전공",
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(top: 5.0),
+                              child: Text(
+                                "UIT 연구원 김득회",
+                                style: TextStyle(fontSize: 18),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ))
+              ],
+            )
+          ],
+        ));
   }
 }
