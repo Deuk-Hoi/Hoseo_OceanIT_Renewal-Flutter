38070e0636947ac3802eef7f52334da0bd59bf3d
Deuk_Hoi <dh97k@naver.com>
2021-01-02 00:29:39 +0900

    separate function

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,211 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  Widget _nameCard() {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      transform: Matrix4.translationValues(0.0, -35.0, 0.0),
+      margin: EdgeInsets.only(right: 20, left: 20),
+      height: 70,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 2.0,
+              spreadRadius: 0.0,
+              offset: Offset(2.0, 2.0),
+            )
+          ]),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            margin: EdgeInsets.only(left: 20.0, right: 16.0),
+            child: CircleAvatar(
+              child: Text("득"),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Text(
+                  "AI 융합대학 컴퓨터공학전공",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                child: Text(
+                  "UIT 연구원 김득회",
+                  style: TextStyle(fontSize: 18),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _researchCard() {
+    return Container(
+        height: 150.0,
+        width: double.infinity,
+        margin: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+        child: Card(
+          elevation: 10.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          child: InkWell(
+              splashColor: Colors.blue.withAlpha(60),
+              onTap: () {},
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    flex: 4,
+                    child: Container(
+                      margin: EdgeInsets.only(left: 15.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10.0),
+                        child: Image.asset(
+                          'assets/OceanMain.jpg',
+                          fit: BoxFit.fill,
+                          width: 140,
+                          height: 100,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                      flex: 6,
+                      child: Container(
+                        height: double.infinity,
+                        margin: EdgeInsets.only(bottom: 20, right: 10),
+                        child: ListTile(
+                          title: Marquee(
+                            child: Text(
+                              "분산형 수중관측 제어망 개발",
+                              style: TextStyle(fontSize: 20, height: 2.5),
+                            ),
+                          ),
+                          subtitle: Text(
+                            "수중환경에서 장시간․실시간 관측 및 제어를 위한 기지국 기반의 수중 관측․제어망 핵심기술 및 체계 구축연구",
+                            maxLines: 3,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      )),
+                ],
+              )),
+        ));
+  }
+
+  Widget _quickMenu() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Card(
+            elevation: 10.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: InkWell(
+                  splashColor: Colors.blue.withAlpha(60),
+                  onTap: () {
+                    print("연구성과");
+                  },
+                  child: Column(
+                    children: [
+                      Icon(
+                        Icons.biotech,
+                        color: Colors.purple[900],
+                        size: 50,
+                      ),
+                      Text("연구성과")
+                    ],
+                  ),
+                ))),
+        Card(
+            elevation: 10.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: InkWell(
+                  splashColor: Colors.blue.withAlpha(60),
+                  onTap: () {
+                    print("연구과제");
+                  },
+                  child: Column(
+                    children: [
+                      Icon(
+                        Icons.create,
+                        color: Colors.purple[900],
+                        size: 50,
+                      ),
+                      Text("연구과제")
+                    ],
+                  ),
+                ))),
+        Card(
+            elevation: 10.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: InkWell(
+                  splashColor: Colors.blue.withAlpha(60),
+                  onTap: () {
+                    print("연구인력");
+                  },
+                  child: Column(
+                    children: [
+                      Icon(
+                        Icons.group,
+                        color: Colors.purple[900],
+                        size: 50,
+                      ),
+                      Text("연구인력")
+                    ],
+                  ),
+                ))),
+        Card(
+            elevation: 10.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: InkWell(
+                  splashColor: Colors.blue.withAlpha(60),
+                  onTap: () {
+                    print("분산형 수증관측망");
+                  },
+                  child: Column(
+                    children: [
+                      Icon(
+                        Icons.waves,
+                        color: Colors.purple[900],
+                        size: 50,
+                      ),
+                      Text("수증관측")
+                    ],
+                  ),
+                ))),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,7 +256,7 @@ class _MainPageState extends State<MainPage> {
                       filled: true,
                       fillColor: Colors.white,
                       hintText: "Search",
-                      hintStyle: TextStyle(height: 3.2)),
+                      hintStyle: TextStyle(height: 3.0)),
                 ),
               ),
               Container(
@@ -62,57 +267,12 @@ class _MainPageState extends State<MainPage> {
                   onPressed: () {},
                 ),
               ),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                margin: EdgeInsets.only(right: 20, left: 20, top: 230),
-                height: 70,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey,
-                        blurRadius: 2.0,
-                        spreadRadius: 0.0,
-                        offset: Offset(2.0, 2.0),
-                      )
-                    ]),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(left: 20.0, right: 16.0),
-                      child: CircleAvatar(
-                        child: Text("득"),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          child: Text(
-                            "AI 융합대학 컴퓨터공학전공",
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        Container(
-                          child: Text(
-                            "UIT 연구원 김득회",
-                            style: TextStyle(fontSize: 18),
-                          ),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              ),
             ],
           ),
+          _nameCard(),
           Container(
             alignment: Alignment.topLeft,
-            margin: EdgeInsets.only(top: 20, left: 20),
+            margin: EdgeInsets.only(left: 20),
             child: Text(
               "최근 연구 과제",
               style: TextStyle(
@@ -121,59 +281,7 @@ class _MainPageState extends State<MainPage> {
                   fontWeight: FontWeight.bold),
             ),
           ),
-          Container(
-              height: 150.0,
-              width: double.infinity,
-              margin: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-              child: Card(
-                elevation: 10.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-                child: InkWell(
-                    splashColor: Colors.blue.withAlpha(60),
-                    onTap: () {},
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Expanded(
-                          flex: 4,
-                          child: Container(
-                            margin: EdgeInsets.only(left: 15.0),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(10.0),
-                              child: Image.asset(
-                                'assets/OceanMain.jpg',
-                                fit: BoxFit.fill,
-                                width: 140,
-                                height: 100,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                            flex: 6,
-                            child: Container(
-                              height: double.infinity,
-                              margin: EdgeInsets.only(bottom: 20, right: 10),
-                              child: ListTile(
-                                title: Marquee(
-                                  child: Text(
-                                    "분산형 수중관측 제어망 개발",
-                                    style: TextStyle(fontSize: 20, height: 2.5),
-                                  ),
-                                ),
-                                subtitle: Text(
-                                  "수중환경에서 장시간․실시간 관측 및 제어를 위한 기지국 기반의 수중 관측․제어망 핵심기술 및 체계 구축연구",
-                                  maxLines: 3,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                              ),
-                            )),
-                      ],
-                    )),
-              )),
+          _researchCard(),
           Container(
             alignment: Alignment.topLeft,
             margin: EdgeInsets.only(top: 10, left: 20, bottom: 20),
@@ -185,103 +293,7 @@ class _MainPageState extends State<MainPage> {
                   fontWeight: FontWeight.bold),
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Card(
-                  elevation: 10.0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: InkWell(
-                        splashColor: Colors.blue.withAlpha(60),
-                        onTap: () {
-                          print("연구성과");
-                        },
-                        child: Column(
-                          children: [
-                            Icon(
-                              Icons.biotech,
-                              color: Colors.purple[900],
-                              size: 50,
-                            ),
-                            Text("연구성과")
-                          ],
-                        ),
-                      ))),
-              Card(
-                  elevation: 10.0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: InkWell(
-                        splashColor: Colors.blue.withAlpha(60),
-                        onTap: () {
-                          print("연구과제");
-                        },
-                        child: Column(
-                          children: [
-                            Icon(
-                              Icons.create,
-                              color: Colors.purple[900],
-                              size: 50,
-                            ),
-                            Text("연구과제")
-                          ],
-                        ),
-                      ))),
-              Card(
-                  elevation: 10.0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: InkWell(
-                        splashColor: Colors.blue.withAlpha(60),
-                        onTap: () {
-                          print("연구인력");
-                        },
-                        child: Column(
-                          children: [
-                            Icon(
-                              Icons.group,
-                              color: Colors.purple[900],
-                              size: 50,
-                            ),
-                            Text("연구인력")
-                          ],
-                        ),
-                      ))),
-              Card(
-                  elevation: 10.0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: InkWell(
-                        splashColor: Colors.blue.withAlpha(60),
-                        onTap: () {
-                          print("분산형 수증관측망");
-                        },
-                        child: Column(
-                          children: [
-                            Icon(
-                              Icons.waves,
-                              color: Colors.purple[900],
-                              size: 50,
-                            ),
-                            Text("수증관측")
-                          ],
-                        ),
-                      ))),
-            ],
-          ),
+          _quickMenu(),
           Container(
             margin: EdgeInsets.only(top: 50),
             alignment: Alignment.bottomCenter,
